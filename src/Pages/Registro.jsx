beda5db74fc033d4aba79fc852f7bade02eca1de
Brandon <bcatanogarcia@gmail.com>
2023-10-19 23:28:06 -0500

    Se inicia formulario de registro utlizando componentes UI y tailwindsCSS

--- src/Pages/Registro.jsx
+++ src/Pages/Registro.jsx
@@ -1,11 +1,26 @@
-
+import Input from "../componentes/ui/Input"
 
 function Registro() {
-  return (
+return (
     <div>
-      Registro
+    <h3 className="text-2xl font-bold">Registro</h3>
+
+    <form>
+        <Input placeholder="Registra nombre" />
+        <Input type="correo" placeholder="Registra correo" />
+        <Input type="celular" placeholder="Registra celular" />
+        <Input type="dependencia" placeholder="Escribe dependencia" />
+        <Input type="dependencia" placeholder="Escribe rol" />
+        <Input type="contrasena" placeholder="Crea tu contraseña" />
+        
+        <button>
+            Registrar
+        </button>
+
+
+    </form>
     </div>
-  )
+    )
 }
 
 export default Registro
